55a8fa40a4123910b7b25e448c0468feb5bfe3f5
Deividas Juska <djuska16@gmail.com>
2023-10-16 15:53:53 +0100

    Add Thing variables and made migrations

--- things/models.py
+++ things/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models import Model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Thing(Model):
+    name = models.CharField(
+        unique=True,
+        max_length=30,
+        blank=False)
+    description = models.CharField(
+        unique=False,
+        blank=True,
+        max_length=120)
+    quantity = models.IntegerField(
+        unique=False,
+        validators=[MaxValueValidator(100), MinValueValidator(0)]
+    )
+
