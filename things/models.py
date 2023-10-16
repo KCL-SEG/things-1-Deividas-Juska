3e062c9ec6faac375ecee8ed205f2b5ca5884939
Deividas Juska <djuska16@gmail.com>
2023-10-16 16:21:04 +0100

    Migrated new values

--- things/models.py
+++ things/models.py
@@ -8,13 +8,13 @@ class Thing(Model):
         max_length=30,
         unique=True,
         blank=False)
-    description = models.TextField(
+    description = models.CharField(
         max_length=120,
         unique=False,
         blank=True)
         
     quantity = models.IntegerField(
         unique=False,
-        validators=[MaxValueValidator(100), MinValueValidator(0)]
-    )
+        validators=[MaxValueValidator(100), MinValueValidator(0)],
+        default=0)
 
